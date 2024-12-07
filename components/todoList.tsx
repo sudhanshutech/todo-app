ad118f6beedcb9e3d6487a299494204e8291b9b6
Sudhanshu Dasgupta <dasguptashivam23@gmail.com>
2024-12-07 14:12:29 +0530

    add empty state

--- components/todoList.tsx
+++ components/todoList.tsx
@@ -24,57 +24,63 @@ const TodoList = ({ date }: { date: string }) => {
         boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)",
       }}
     >
-      {filteredTodos.map((todo) => (
-        <div
-          key={todo.id}
-          className={`flex justify-between items-center p-2 ${
-            todo.completed ? "line-through text-gray-500" : ""
-          }`}
-          style={{
-            backgroundColor: "#fff",
-            boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)",
-            borderRadius: "5px",
-            marginBottom: "5px",
-            padding: "15px",
-          }}
-        >
-          <div className="flex items-center">
-            <div>
-              <input
-                type="checkbox"
-                checked={todo.completed}
-                onChange={() => toggleTodo(todo.id)}
-                className="mr-2"
-              />
-            </div>
-            <div>
-              <h3 className="font-semibold">{todo.title}</h3>
-              <span />
-              {todo.text}
-            </div>
-          </div>
-          <div className="flex items-center space-x-2">
-            <IconButton
-              sx={{
-                padding: "3px",
-              }}
-              onClick={() => setEditingTodoId(todo.id)}
-              className="text-blue-500 hover:underline"
-            >
-              <Edit />
-            </IconButton>
-            <IconButton
-              sx={{
-                padding: "3px",
-              }}
-              onClick={() => deleteTodo(todo.id)}
-              className="text-red-500 hover:underline"
-            >
-              <Delete />
-            </IconButton>
-          </div>
+      {filteredTodos.length === 0 ? (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100%" }}>
+          <p className="text-gray-500">No tasks created</p>
         </div>
-      ))}
+      ) : (
+        filteredTodos.map((todo) => (
+          <div
+            key={todo.id}
+            className={`flex justify-between items-center p-2 ${
+              todo.completed ? "line-through text-gray-500" : ""
+            }`}
+            style={{
+              backgroundColor: "#fff",
+              boxShadow: "0 0 5px rgba(0, 0, 0, 0.1)",
+              borderRadius: "5px",
+              marginBottom: "5px",
+              padding: "15px",
+            }}
+          >
+            <div className="flex items-center">
+              <div>
+                <input
+                  type="checkbox"
+                  checked={todo.completed}
+                  onChange={() => toggleTodo(todo.id)}
+                  className="mr-2"
+                />
+              </div>
+              <div>
+                <h3 className="font-semibold">{todo.title}</h3>
+                <span />
+                {todo.text}
+              </div>
+            </div>
+            <div className="flex items-center space-x-2">
+              <IconButton
+                sx={{
+                  padding: "3px",
+                }}
+                onClick={() => setEditingTodoId(todo.id)}
+                className="text-blue-500 hover:underline"
+              >
+                <Edit />
+              </IconButton>
+              <IconButton
+                sx={{
+                  padding: "3px",
+                }}
+                onClick={() => deleteTodo(todo.id)}
+                className="text-red-500 hover:underline"
+              >
+                <Delete />
+              </IconButton>
+            </div>
+          </div>
+        ))
+      )}
       {editingTodoId && (
         <EditTodoModal
           isOpen={!!editingTodoId}
